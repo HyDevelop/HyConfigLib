558698da132855fdb6dd15c6201bd7702e1259a0
Hykilpikonna <admin@moecraft.cc>
2018-10-04 11:51:26 -0400

    [+] Util method to create a file

--- src/main/java/cc/moecraft/yaml/ConfigUtils.java
+++ src/main/java/cc/moecraft/yaml/ConfigUtils.java
@@ -14,6 +14,42 @@ public class ConfigUtils
 {
     public static boolean debug = false;
 
+    /**
+     * Create a file
+     *
+     * @param file The file object of the actual file in storage to create.
+     * @return Success or not.
+     */
+    static boolean createFile(File file)
+    {
+        File absoluteFile = file.getAbsoluteFile();
+
+        if(absoluteFile.exists()) log("Failed to create file: File " + absoluteFile + " already exists.");
+        else if (absoluteFile.isDirectory()) log("Failed to create file: File " + absoluteFile + "is a directory.");
+        else
+        {
+            // Detect if parent dir exists.
+            if(!absoluteFile.getParentFile().exists())
+                if(!absoluteFile.getParentFile().mkdirs())
+                    error("Failed to create file: " + absoluteFile + ": Failed to create parent directory.");
+
+            // Create the actual file.
+            try
+            {
+                if (absoluteFile.createNewFile())
+                {
+                    log("File " + absoluteFile + " is successfully created.");
+                    return true;
+                }
+                else error("Failed to create file " + absoluteFile + ". Reason Unknown.");
+            }
+            catch (Exception e)
+            {
+                throw new RuntimeException("[HyConfig] Failed to create file " + absoluteFile + ".", e);
+            }
+        }
+        return false;
+    }
 
     private static void error(String msg)
     {
