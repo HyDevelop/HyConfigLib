8cbfc6c6890c68ecc128445de582d41f8ed62b1a
Hykilpikonna <admin@moecraft.cc>
2018-10-07 09:53:00 -0400

    [+] Add base config class

---
+++ src/main/java/cc/moecraft/yaml/HyConfigBase.java
@@ -0,0 +1,48 @@
+package cc.moecraft.yaml;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/07 创建!
+ * Created by Hykilpikonna on 2018/10/07!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public abstract class HyConfigBase extends YamlConfiguration
+{
+    /**
+     * Package-private no args constructor.
+     */
+    HyConfigBase() {}
+
+    /**
+     * Get keys in a path.
+     *
+     * Example:
+     *   KeyA:
+     *     SubKey1: "TestValue"
+     *     SubKey2: "TestValue2"
+     *   KeyB: "TestValue3"
+     *
+     *   getKeys("") will return ["KeyA", "KeyB"]
+     *   getKeys("KeyA") will return ["SubKey1", "SubKey2"]
+     *
+     * @param path Path of the parent key.
+     * @return Keys under the parent key.
+     */
+    public ArrayList<String> getKeys(String path)
+    {
+        try
+        {
+            return new ArrayList<>(getConfigurationSection(path).getKeys(false));
+        }
+        catch (NullPointerException e)
+        {
+            return new ArrayList<>();
+        }
+    }
+}
