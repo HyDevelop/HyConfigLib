760341eed2af7230804a00dcb05022b2c5d8ccdf
Hykilpikonna <admin@moecraft.cc>
2018-10-07 09:53:36 -0400

    [+] Virtual config

---
+++ src/main/java/cc/moecraft/yaml/HyVirtualConfig.java
@@ -0,0 +1,27 @@
+package cc.moecraft.yaml;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+
+import java.io.File;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/07 创建!
+ * Created by Hykilpikonna on 2018/10/07!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class HyVirtualConfig extends HyConfigBase
+{
+    public HyVirtualConfig(String config) throws InvalidConfigurationException
+    {
+        super.loadFromString(config);
+    }
+
+    @Override
+    public String toString()
+    {
+        return saveToString();
+    }
+}
